4f80ae7657ee9e5d90a822d5443d27cd46f4f017
AndreyNovaes <andreynovaespro@gmail.com>
2023-05-02 22:45:45 -0300

    REFACTOR: change the spiders based on item loader logic

--- scrapyProject/spiders/buscape_spider.py
+++ scrapyProject/spiders/buscape_spider.py
@@ -1,29 +1,27 @@
 import scrapy
-from ..Generate_urls import Generate_urls
-import scrapyProject.items as items
-import os
+from scrapyProject.items.buscape_item import BuscapeItem
+from scrapyProject.item_loaders import BuscapeItemLoader
+from scrapyProject.urls_manager import generate_urls_buscape, check_url_status
 
 class BuscapeSpider(scrapy.Spider):
   name = "buscape"
-  instancia_generate_urls = Generate_urls()
-  
-  start_urls = instancia_generate_urls.generate_urls_buscape(["celular", "geladeira", 'tv'], 50)
+  categories = ["celular", "tv", "geladeira"]
+  number_of_pages = 41
+  urls = generate_urls_buscape(categories, number_of_pages)
+  start_urls = check_url_status(urls)
 
   def parse(self, response):
     products = response.css(".SearchCard_ProductCard_Inner__7JhKb")
     category = response.url.split("/")[3].split("?")[0]
 
     for product in products:
-      item = items.ScrapyprojectItem()
+      item_loader = BuscapeItemLoader(item=BuscapeItem(), selector=product)
+      item_loader.add_css('description', 'h2::text')
+      item_loader.add_css('image', 'img[src^="https"]::attr(src)')
+      item_loader.add_css('price', "p[data-testid = 'product-card::price']::text")
+      item_loader.add_css('link', 'a::attr(href)')
 
-      price_str = product.css("p[data-testid = 'product-card::price']::text").get()
-      price_decimal = float(price_str.replace("R$", "").replace(".", "").replace(",", "."))      
+      item_loader.add_value('category', category)
+      item_loader.add_value('website', 'Buscape')
 
-      item["description"] = product.css("h2::text").get()
-      item["price"] = price_decimal
-      item["image"] = product.css("img[src^='https']").attrib["src"]
-      item["link"] = Generate_urls.mountUrl("https://www.buscape.com.br", product.css("a::attr(href)").get())
-      item["category"] = category
-      item["website"] = "Buscape"
-
-      yield item
+      yield item_loader.load_item()
--- scrapyProject/spiders/meli_spider.py
+++ scrapyProject/spiders/meli_spider.py
@@ -1,38 +1,37 @@
 import scrapy
-import scrapyProject.items as items
-
-from ..Generate_urls import Generate_urls
+from scrapyProject.items.meli_item import MeliItem
+from scrapyProject.item_loaders import MeliItemLoader
+from scrapyProject.urls_manager import generate_urls_meli, check_url_status
 
 class MeliSpiderSpider(scrapy.Spider):
   name = "meli"
-  instancia_generate_urls = Generate_urls()
   categories = [
     '/eletrodomesticos/refrigeracao/geladeiras/geladeira',
     '/celulares-telefones/celulares-smartphones/celulares-smartphones',
     '/eletronicos-audio-video/televisores/tv'
     ]
-  
-  start_urls = instancia_generate_urls.generate_urls_meli(categories, 50)
+  number_of_pages = 45
+  urls = generate_urls_meli(categories, number_of_pages)
+  start_urls = check_url_status(urls)
 
   def parse(self, response):
     products = response.xpath('//li[contains(@class, "ui-search-layout__item")]')
     category = response.url.split('/')[-1].split('_')[0]
 
     for product in products:
-      item = items.ScrapyprojectItem()
-      
-      price_str = product.css("span.price-tag-amount > span.price-tag-fraction::text").get()
-      price_decimal = float(price_str.replace(".", "").replace(",", "."))
-      
-      item["image"] = product.css("img.ui-search-result-image__element::attr(data-src)").get()
-      item["price"] = price_decimal
-      item["description"] = product.css("h2.ui-search-item__title::text").get()
-      item["link"] = product.css("a.ui-search-link::attr(href)").get()
-      item["website"] = "Mercado Livre"
-      
-      if category == "celulares-smartphones":
-        item["category"] = "celular"
-      else:
-        item["category"] = category
+      item_loader = MeliItemLoader(item=MeliItem(), selector=product)
 
-      yield item
+      item_loader.add_css('description', 'h2.ui-search-item__title::text')
+      item_loader.add_css('image', 'img.ui-search-result-image__element::attr(data-src)')
+      item_loader.add_css('link', 'a.ui-search-link::attr(href)')
+      price_selector = (
+        "span.price-tag.ui-search-price__part:not(.ui-search-price__original-value) > "
+        "span.price-tag-amount > "
+        "span.price-tag-fraction::text"
+      )
+      item_loader.add_css('price', price_selector)
+      
+      item_loader.add_value('category', category)
+      item_loader.add_value('website', 'Mercado Livre')
+
+      yield item_loader.load_item()
