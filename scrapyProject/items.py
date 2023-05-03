2bb8ca9e8d935758111d54fec71ec09b767944d5
AndreyNovaes <andreynovaespro@gmail.com>
2023-05-02 22:42:31 -0300

    REFACTOR: add item_loader to items logic, and move to items folder for a better organization

--- scrapyProject/items.py
+++
@@ -1,9 +0,0 @@
-import scrapy
-
-class ScrapyprojectItem(scrapy.Item):
-  category = scrapy.Field()
-  description = scrapy.Field()
-  price = scrapy.Field()
-  image = scrapy.Field()
-  link = scrapy.Field()
-  website = scrapy.Field()
