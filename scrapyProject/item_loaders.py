4b0756b8b38709fd102ba9eab9ceee02819131e7
AndreyNovaes <andreynovaespro@gmail.com>
2023-05-04 21:08:42 -0300

    feat: add process_base64_image to item_loaders

--- scrapyProject/item_loaders.py
+++ scrapyProject/item_loaders.py
@@ -1,6 +1,6 @@
 from scrapy.loader import ItemLoader
 from itemloaders.processors import MapCompose, TakeFirst
-from scrapyProject.utils import handle_meli_compost_category_name, remove_html_tags_and_whitespaces, remove_currency_symbol, convert_to_float
+from scrapyProject.utils import handle_meli_compost_category_name, process_base64_image, remove_html_tags_and_whitespaces, remove_currency_symbol, convert_to_float
 from scrapyProject.urls_manager import mount_relative_urls_buscape
 
 class BuscapeItemLoader(ItemLoader):
@@ -15,4 +15,5 @@ class MeliItemLoader(ItemLoader):
     category_in = MapCompose(remove_html_tags_and_whitespaces, handle_meli_compost_category_name)
     price_in = MapCompose(remove_html_tags_and_whitespaces, convert_to_float)
     link_in = MapCompose(remove_html_tags_and_whitespaces, mount_relative_urls_buscape)
+    image_in = MapCompose(remove_html_tags_and_whitespaces, process_base64_image)
     
